a91cef6be767dc1488557fc676411dad7f8af0e6
bore helu <borehelu@gmail.com>
2022-08-28 01:23:01 +0300

    added score component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import Score from "./components/Score";
+
 function App() {
+  const [score, setScore] = useState(0);
+
+  function increaseScore(){
+    setScore(prevScore => prevScore + 1);
+
+  }
+
+  function decreaseScore(){
+    setScore(prevScore => {
+      if(prevScore !== 0){
+        return prevScore -1;
+      }else{
+        return 0;
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello world</h1>
+      <Score score={score}/>
     </div>
   );
 }
---
+++ src/components/Score.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Score({score}) {
+  return (
+    <div className="score">
+        <div className="logo">
+            <img src="/images/logo.svg" alt="logo" />
+        </div>
+        <div className="score__details">
+            <h1>score</h1>
+            <p>{score}</p>
+        </div>
+    </div>
+  )
+}
+
+export default Score
