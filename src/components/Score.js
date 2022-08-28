0c6d80970e086438d00cafa19a6c7466a6728a37
bore helu <borehelu@gmail.com>
2022-08-29 00:02:16 +0300

    updated path to logo

--- src/components/Score.js
+++ src/components/Score.js
@@ -6,7 +6,7 @@ function Score({score}) {
   return (
     <div className="score">
         <div className="logo">
-            <img src="/images/logo.svg" alt="logo" />
+            <img src="images/logo.svg" alt="logo" />
         </div>
         <div className="score__details">
             <h1>score</h1>
